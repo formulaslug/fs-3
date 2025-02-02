f7e8d096de20b449fde5ed83c229589125af7b7f
Jack N <nystromjp@gmail.com>
2025-02-01 20:56:40 -0800

    save

--- LiveTelemetry/main.cpp
+++ LiveTelemetry/main.cpp
@@ -7,7 +7,7 @@
 
 #define BUF_SIZE 1000
 
-static BufferedSerial radio_serial(D5, D4);
+static BufferedSerial radio_serial(PA_9, PA_10);
 static BufferedSerial usb_serial(USBTX, USBRX);
 static DigitalOut led(LED1);
 
@@ -19,29 +19,29 @@ int main()
         /* parity */ BufferedSerial::None,
         /* stop bit */ 1
     );
+    radio_serial.set_blocking(false);
     usb_serial.set_baud(9600);
     usb_serial.set_format(
         /* bits */ 8,
         /* parity */ BufferedSerial::None,
         /* stop bit */ 1
     );
+    usb_serial.set_blocking(false);
 
-    char w[] = "hellwo woasda\n";
-    printf("%s", w);
-    usb_serial.write(w, strlen(w));
+    // printf("hellwo woasda\n");
 
     char buf[BUF_SIZE] = {0};
 
-    thread_sleep_for(2000);
-    char at_command_mode[] = "+++";
-    radio_serial.write(at_command_mode, sizeof(at_command_mode));
-    thread_sleep_for(2000);
+    // thread_sleep_for(2000);
+    // char at_command_mode[] = "+++";
+    // radio_serial.write(at_command_mode, sizeof(at_command_mode));
+    // thread_sleep_for(2000);
 
 
     while (true) {
         if (uint32_t num = usb_serial.read(buf, sizeof(buf))) {
             led = !led;
-            /* usb_serial.write(buf, num); */
+            // usb_serial.write(buf, num);
             radio_serial.write(buf, num);
         }
         
--- LiveTelemetryGround/main.cpp
+++ LiveTelemetryGround/main.cpp
@@ -7,28 +7,18 @@
 
 #define BUF_SIZE 1000
 
-static BufferedSerial radio_serial(A7, D12);
+static BufferedSerial radio_serial(PA_9, PA_10);
 static BufferedSerial usb_serial(USBTX, USBRX);
 static DigitalOut led(LED1);
 
 int main()
 {
     radio_serial.set_baud(9600);
-    radio_serial.set_format(
-        /* bits */ 8,
-        /* parity */ BufferedSerial::None,
-        /* stop bit */ 1
-    );
+    radio_serial.set_format(8, BufferedSerial::None, 1);
     usb_serial.set_baud(9600);
-    usb_serial.set_format(
-        /* bits */ 8,
-        /* parity */ BufferedSerial::None,
-        /* stop bit */ 1
-    );
+    usb_serial.set_format(8, BufferedSerial::None, 1);
 
-    char w[] = "hello world\n";
-    printf("%s", w);
-    usb_serial.write(w, strlen(w));
+    /* printf("hello world!\n"); */
 
     char buf[BUF_SIZE] = {0};
 
