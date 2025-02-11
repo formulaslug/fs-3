a73d8961f52b696728c28613513278805b01630d
Jonathan Uhler <jonathan@uhler.com>
2025-02-10 23:00:26 -0800

    add test framework for ETC

---
+++ ETC/tests/example_test.h
@@ -0,0 +1,14 @@
+#ifndef _EXAMPLE_TEST_H_
+#define _EXAMPLE_TEST_H_
+
+
+#include "unity.h"
+
+
+void some_example_test() {
+    TEST_ASSERT_EQUAL(4, 2 * 2);
+    TEST_ASSERT_EQUAL(3, 1 + 1);
+}
+
+
+#endif  // _EXAMPLE_TEST_H_
---
+++ ETC/tests/test_main.cpp
@@ -0,0 +1,69 @@
+/**
+ * @file test_main.cpp
+ *
+ * The main runner file for ETC unit tests. (This file does not test {@code main.cpp}).
+ *
+ * @author Jonathan Uhler
+ */
+
+
+// Unity allows for a `unity_config.h` header file for programmer-defined configuration. If we
+// don't have/use this, we don't need to include it.
+#undef UNITY_INCLUDE_CONFIG_H
+
+
+// Include other test files here. Remember to add test cases to the "ETC_TEST_CASES" array!
+#include "example_test.h"
+
+// Standard headers begin here
+#include "mbed.h"
+#include "unity.h"
+#include <iostream>
+
+
+/**
+ * Add tests here.
+ */
+void run_all_tests() {
+    RUN_TEST(some_example_test);
+}
+
+
+/**
+ * Set up function for Unity tests.
+ *
+ * DO NOT MODIFY! If you are just adding new tests, read the header comment in this file.
+ */
+void setUp() {
+    // Do nothing
+}
+
+
+/**
+ * Teardown function for Unity tests.
+ *
+ * DO NOT MODIFY! If you are just adding new tests, read the header comment in this file.
+ */
+void tearDown() {
+    // Do nothing
+}
+
+
+/**
+ * Entry point for running tests.
+ *
+ * DO NOT MODIFY! If you are just adding new tests, read the header comment in this file.
+ *
+ * @return A zero status code if all tests pass, and non-zero if any test failed.
+ */
+int main() {
+    UNITY_BEGIN();
+    run_all_tests();
+    int status_code = UNITY_END();
+
+    while (true) {
+        std::cout << "Status code: " << status_code << std::endl;
+    }
+
+    return 0;
+}
