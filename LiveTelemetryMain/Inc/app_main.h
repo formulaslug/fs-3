00e3d1bf17eb1f1c50d80089e8efcc82519476c2
Jack N <nystromjp@gmail.com>
2025-04-09 19:54:39 -0700

    added LiveTelemetry[Receiver,Main]

---
+++ LiveTelemetryMain/Inc/app_main.h
@@ -0,0 +1,3 @@
+#include "stm32f4xx_hal_uart.h"
+
+int app_main(SPI_HandleTypeDef hspi, UART_HandleTypeDef huart2);
--- LiveTelemetryReceiver/main.cpp
+++ LiveTelemetryReceiver/main.cpp
@@ -5,9 +5,24 @@
 
 #include "mbed.h"
 
+SPI spi(PA_7, PA_6, PA_5); // mosi, miso, sclk
+DigitalOut cs(PB_0);
 
 int main()
 {
+    cs.write(1);
+
+    spi.format(8, 0);
+    spi.frequency(1000000);
+
+    cs.write(0);
+
+    
+
+    spi.write()
+
+    cs.write(1);
+
     printf("Hello, Mbed!\n");
     return 0;
 }
