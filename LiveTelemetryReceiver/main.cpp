32cb142c1ec0358ea6bf70bb76202955f6ea4836
Jack N <nystromjp@gmail.com>
2025-04-12 15:49:57 -0700

    testing...

--- LiveTelemetryReceiver/main.cpp
+++ LiveTelemetryReceiver/main.cpp
@@ -5,6 +5,7 @@
 
 #include "mbed.h"
 
+DigitalIn spi_attn(PB_6);
 DigitalOut cs(PB_0);
 SPI spi(PA_7, PA_6, PA_5); // mosi, miso, sclk[, ss]
 
@@ -22,45 +23,65 @@ bool to_hex(char *dest, size_t dest_len, const uint8_t *values,
   return true;
 }
 
+char str[100] = {0};
+
 int main() {
   // cs.write(1);
 
   spi.format(8, 0);
   spi.frequency(1000000);
+  spi.set_default_write_value(0x00);
 
   while (1) {
     // HAL_GPIO_WritePin(GPIOC, 8, 1);
     const uint8_t local_at_command[] = {
-        0x7E, // Start delimeter
-        0x00, // LEN - MSB
-        0x04, // LEN - LSB
-
-        // ---- BEGIN DATA ----
-        0x08,       // frametype
-        0x77,       // frameid
-        0x53, 0x48, // AT command - "SH"
-        // (no parameter in this example)
-        // ---- END DATA ----
-
-        (0xFF - ((0x08 + 0x77 + 0x53 + 0x48) & 0xFF)), // Checksum
-        // 0x7E, 0x00, 0x04, 0x08, 0x17, 0x54, 0x50, 0x3C,
+        // 0x7E, // Start delimeter
+        // 0x00, // LEN - MSB
+        // 0x04, // LEN - LSB
+        //
+        // // ---- BEGIN DATA ----
+        // 0x08,       // frametype
+        // 0x77,       // frameid
+        // 0x53, 0x48, // AT command - "SH"
+        // // (no parameter in this example)
+        // // ---- END DATA ----
+        //
+        // (0xFF - ((0x08 + 0x77 + 0x53 + 0x48) & 0xFF)), // Checksum
+        0x7E, 0x00, 0x04, 0x08, 0x17, 0x54, 0x50, 0x3C,
     };
 
-    cs.write(0);
+    uint8_t resp_buf[10] = {0};
 
-    uint8_t resp_buf[1024] = {0};
+    // cs.write(0);
 
-    spi.write((char *)local_at_command, sizeof(local_at_command),
-              (char *)resp_buf, sizeof(resp_buf));
-    // spi.transfer(const Type *tx_buffer, int tx_length, Type *rx_buffer, int rx_length, const event_callback_t &callback)
+    // spi.write((char *)local_at_command, sizeof(local_at_command),
+    //           (char *)resp_buf, sizeof(resp_buf));
 
-    cs.write(1);
+    // spi.transfer(local_at_command, sizeof(local_at_command), resp_buf,
+    // sizeof(resp_buf), [&](int _) {
+    //   to_hex(str, sizeof(str), resp_buf, sizeof(resp_buf));
+    // });
+    //
+    // while (spi_attn.read()==1) {}
+    // cs.write(1);
+    //
+
+    if (!spi_attn) {
+      cs.write(0);
+      spi.write(NULL, 0, (char *)resp_buf, sizeof(resp_buf));
+      cs.write(1);
+    } else {
+      cs.write(0);
+      spi.write((char *)local_at_command, sizeof(local_at_command),
+                (char *)resp_buf, sizeof(resp_buf));
+      cs.write(1);
+    }
 
     HAL_Delay(10);
     char str[1024] = {0};
     to_hex(str, sizeof(str), resp_buf, sizeof(resp_buf));
 
-    printf("0x%s", str);
+    printf("0x%s, %d", str, spi_attn.read());
     printf("\n");
   }
 
