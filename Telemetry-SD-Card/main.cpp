264d1cc4c79c3a8e22ab8571efc8801e5fa2dd50
goob10000 <65315624+goob10000@users.noreply.github.com>
2025-04-21 15:25:27 -0700

    starting code

--- Telemetry-SD-Card/main.cpp
+++ Telemetry-SD-Card/main.cpp
@@ -1,13 +1,15 @@
-#include "mbed.h"
 #include "SDFileSystem.h"
+#include "mbed.h"
 
-int main()
-{
-    while(true) {}
-
-    // main() is expected to loop forever.
-    // If main() actually returns the processor will halt
+int main() {
+  SDFileSystem f{};
+  while (true) {
     return 0;
-}//
+  }
+
+  // main() is expected to loop forever.
+  // If main() actually returns the processor will halt
+  return 0;
+} //
 // Created by Goob on 4/21/2025.
 //
