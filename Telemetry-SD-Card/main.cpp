af5aa6eca75da607d700b0f2d09ed953071427c3
goob10000 <65315624+goob10000@users.noreply.github.com>
2025-04-21 14:49:36 -0700

    Finished project setup

---
+++ Telemetry-SD-Card/main.cpp
@@ -0,0 +1,13 @@
+#include "mbed.h"
+#include "SDFileSystem.h"
+
+int main()
+{
+    while(true) {}
+
+    // main() is expected to loop forever.
+    // If main() actually returns the processor will halt
+    return 0;
+}//
+// Created by Goob on 4/21/2025.
+//
