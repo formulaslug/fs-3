f750356c9a53b0ba2800b9ea1e3d848941b0da13
goob10000 <65315624+goob10000@users.noreply.github.com>
2025-04-21 18:07:25 -0700

    sd card beginnings

--- Telemetry-SD-Card/main.cpp
+++ Telemetry-SD-Card/main.cpp
@@ -1,12 +1,15 @@
 #include "SDFileSystem.h"
 #include "mbed.h"
+#include <iostream>
 
 int main() {
-  SDFileSystem f{};
-  while (true) {
-    return 0;
-  }
-
+  SDFileSystem fs{D2, A5, A4, D3, "sd"}; // Mosi, miso, sclk, cs
+  fs.disk_initialize();
+  printf("File system initialized\n");
+  const auto file = fs.open("sd", FA_WRITE | FA_CREATE_ALWAYS);
+  file->write("Hello_world", 12);
+  printf("Closing File\n");
+  file->close();
   // main() is expected to loop forever.
   // If main() actually returns the processor will halt
   return 0;
