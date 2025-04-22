d1aaa967abef290ed8b1dd04b90389ea50863541
Jack N <nystromjp@gmail.com>
2025-04-22 16:49:40 -0700

    update SD code to use c++17 std::filesystem

--- Telemetry-SD-Card/main.cpp
+++ Telemetry-SD-Card/main.cpp
@@ -1,34 +1,24 @@
 #include "SDFileSystem.h"
 #include "mbed.h"
+#include <filesystem>
 #include <iostream>
+#include <fstream>
+
+namespace fs = std::filesystem;
 
 int main() {
-  SDFileSystem fs{D2, A5, A4, D3, "sd"}; // Mosi, miso, sclk, cs
+  SDFileSystem sd{D2, A5, A4, D3, "sd"}; // Mosi, miso, sclk, cs
 
   printf("Hello World!\n");   
 
-  mkdir("/sd/mydir", 0777);
-  
-  FILE *fp = fopen("/sd/mydir/sdtest.txt", "w");
-  if(fp == NULL) {
-      error("Could not open file for write\n");
-  }
-  fprintf(fp, "Hello World!");
-  fclose(fp); 
+  fs::create_directory("/sd/dir2");
+  fs::path file("/sd/dir2/test.txt");
+
+  std::ofstream out_stream(file);
+  out_stream << "asjdlkajsdklajs" << std::endl;
 
   printf("Goodbye World!\n");
 
-  // fs.disk_initialize();
-  //
-  // fs.
-  //
-  // printf("File system initialized\n");
-  //
-  // FileHandle *file = fs.open("test", FA_WRITE); // | FA_CREATE_NEW
-  //
-  // file->write("Hello_world", 13);
-  //
-  // printf("Closing File\n");
-  // file->close();
+
   return 0;
 }
