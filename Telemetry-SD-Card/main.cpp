6fe408911ffd937196937855577fdb7b210fc2d4
Jack N <nystromjp@gmail.com>
2025-04-21 19:03:22 -0700

    working SD example code!

--- Telemetry-SD-Card/main.cpp
+++ Telemetry-SD-Card/main.cpp
@@ -4,15 +4,31 @@
 
 int main() {
   SDFileSystem fs{D2, A5, A4, D3, "sd"}; // Mosi, miso, sclk, cs
-  fs.disk_initialize();
-  printf("File system initialized\n");
-  const auto file = fs.open("sd", FA_WRITE | FA_CREATE_ALWAYS);
-  file->write("Hello_world", 12);
-  printf("Closing File\n");
-  file->close();
-  // main() is expected to loop forever.
-  // If main() actually returns the processor will halt
+
+  printf("Hello World!\n");   
+
+  mkdir("/sd/mydir", 0777);
+  
+  FILE *fp = fopen("/sd/mydir/sdtest.txt", "w");
+  if(fp == NULL) {
+      error("Could not open file for write\n");
+  }
+  fprintf(fp, "Hello World!");
+  fclose(fp); 
+
+  printf("Goodbye World!\n");
+
+  // fs.disk_initialize();
+  //
+  // fs.
+  //
+  // printf("File system initialized\n");
+  //
+  // FileHandle *file = fs.open("test", FA_WRITE); // | FA_CREATE_NEW
+  //
+  // file->write("Hello_world", 13);
+  //
+  // printf("Closing File\n");
+  // file->close();
   return 0;
-} //
-// Created by Goob on 4/21/2025.
-//
+}
