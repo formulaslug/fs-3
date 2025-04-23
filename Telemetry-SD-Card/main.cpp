e1d0e3f49c81a7f3db75923e74ebec8279ad4064
goob10000 <65315624+goob10000@users.noreply.github.com>
2025-04-22 18:06:18 -0700

    testing mbed-os root with linking errors

--- Telemetry-SD-Card/main.cpp
+++ Telemetry-SD-Card/main.cpp
@@ -3,19 +3,21 @@
 #include <filesystem>
 #include <iostream>
 #include <fstream>
+#include <wchar.h>
+
 
 namespace fs = std::filesystem;
 
 int main() {
   SDFileSystem sd{D2, A5, A4, D3, "sd"}; // Mosi, miso, sclk, cs
 
-  printf("Hello World!\n");   
+  printf("Hello World!\n");
 
   fs::create_directory("/sd/dir2");
   const fs::path file("/sd/dir2/test.txt");
 
   std::ofstream out_stream(file);
-  out_stream << "asjdlkajsdklajs" << std::endl;
+  // out_stream << "asjdlkajsdklajs" << std::endl;
 
   printf("Goodbye World!\n");
 
