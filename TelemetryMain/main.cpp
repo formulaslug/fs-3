af376d40a6b52b95d2076d6350d490f9ae817c93
Jack N <nystromjp@gmail.com>
2025-05-06 03:10:05 -0700

    save

--- TelemetryMain/main.cpp
+++ TelemetryMain/main.cpp
@@ -2,7 +2,14 @@
 
 CAN *can;
 
+DigitalOut led(LED1);
+
 int main() {
+  // while (1) {
+  //   led.write(!led.read());
+  //   ThisThread::sleep_for(100ms);
+  //   printf("hi\n");
+  // }
   printf("v1.0\n");
   can = new CAN(PB_8, PB_9, 500000);
   CANMessage msg;
@@ -11,6 +18,7 @@ int main() {
       printf("%x: %x %x %x %x %x %x %x %x\n", msg.id, msg.data[0], msg.data[1],
              msg.data[2], msg.data[3], msg.data[4], msg.data[5], msg.data[6],
              msg.data[7]);
+      led.write(!led.read());
     }
   }
 
--- TelemetryPeripheral/main.c
+++ TelemetryPeripheral/main.c
@@ -8,6 +8,8 @@
 #include "mcc_generated_files/mcc.h"
 #include <avr/io.h>
 
+// #include "MCP_CAN_lib-master/mcp_can.hpp"
+
 
 int main() {
 
@@ -20,7 +22,24 @@ int main() {
 
   IO_PC3_SetLow();
 
-  char buf[] = {0b00000010, 0x28, 0b0000001, 0b10000000, 0b00000100 };
+  char clear[] = {0b00000010, 0b00110000, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+  SPI0_WriteBlock(clear, sizeof(clear));
+
+  IO_PC3_SetHigh();
+
+  SPI0_Close();
+
+  DELAY_milliseconds(10);
+
+  SPI0_OpenConfiguration(MASTER0_CONFIG);
+
+  IO_PC3_SetLow();
+
+  //             WRITE       ADDR  CNF3        CNF2        CNF1
+  char buf[] = { 0b00000010, 0x28, 0b00000001, 0b10000000, 0b00000100 }; // from our old peripheral board revision (math done with Cole)
+  // char buf[] = { 0b00000010, 0x28, 0x81, 0xD1, 0x00 }; // from the arduino mcp_can library
+
+  // char buf[] = { 0b00000010, 0x28, 0b00000001, 0b10001010, 0b00000000 }; // new code
   SPI0_WriteBlock(buf, sizeof(buf));
 
   IO_PC3_SetHigh();
@@ -44,7 +63,7 @@ int main() {
 
     // clang-format: ignore
     //            WRITE       ADDR        CTRL        SIDH        SIDL        EID8  EID0  DLC         DATA                                           
-    char buf[] = {0b00000010, 0b00110000, 0b00001011, 0b01010101, 0b01000000, 0x00, 0x00, 0b00001000, 0xDE, 0xAD, 0xBE, 0xEF, 0xDE, 0xAD, 0xBE, 0xEF, 0x00, 0x00};
+    char buf[] = {0b00000010, 0b00110000, 0b00001011, 0b10101010, 0b01000000, 0x00, 0x00, 0b00001000, 0xDE, 0xAD, 0xBE, 0xEF, 0xDE, 0xAD, 0xBE, 0xEF, 0x00, 0x00};
     SPI0_WriteBlock(buf, sizeof(buf));
 
     IO_PC3_SetHigh();
