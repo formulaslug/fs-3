a3ed48d4e4b1012371154ad812ebe4030fce16ff
Jack N <nystromjp@gmail.com>
2025-05-14 19:54:40 -0700

    Get SPI working on receiver and main board + added XBeeConfigurator project
    configurator uses uart - probably not useful much now that spi works but kept for archival anyways

--- LiveTelemetryMain/main.cpp
+++ LiveTelemetryMain/main.cpp
@@ -2,114 +2,69 @@
  * Copyright (c) 2025 ARM Limited
  * SPDX-License-Identifier: Apache-2.0
  */
-
 #include "mbed.h"
 
-DigitalIn spi_attn(PB_6);
-DigitalOut cs(PB_0);
-SPI spi(PA_7, PA_6, PA_5); // mosi, miso, sclk[, ss]
-
-DigitalOut reset(PA_4);
-DigitalOut dout(PA_3);
-
-DigitalOut orange(A0);
-DigitalOut red(A1);
-BufferedSerial test_serial(orange, red);
-
-
-bool to_hex(char *dest, size_t dest_len, const uint8_t *values,
-            size_t val_len) {
-  if (dest_len < (val_len * 2 + 1)) /* check that dest is large enough */
-    return false;
-  *dest = '\0'; /* in case val_len==0 */
-  while (val_len--) {
-    /* sprintf directly to where dest points */
-    sprintf(dest, "%02X", *values);
-    dest += 2;
-    ++values;
-  }
-  return true;
-}
-
-char str[100] = {0};
+DigitalIn spi_attn(PA_9);
+DigitalOut cs(PC_8);
+SPI spi(PA_7, PA_6, PA_5);
 
 int main() {
+  printf("Main() - LiveTelemetryMain");
   cs.write(1);
 
-  
+  spi.format(8, 0);
+  spi.frequency(1000000);
+  spi.set_default_write_value(0x00);
 
-  // spi.format(8, 0);
-  // spi.frequency(1000000);
-  // spi.set_default_write_value(0x00);
+  ThisThread::sleep_for(200ms);
+  while (true) {
+    // Example directly out of datasheet
+    const uint8_t at_command_TP[] = {
+        0x7E, 0x00, 0x04, 0x08, 0x17, 0x54, 0x50, 0x3C, // AT command - "TP"
+    };
+    uint8_t resp_buf[11] = {0}; // TP returns an 11-byte frame
 
-  printf("%d", spi_attn.read());
-  dout = 0;
-  reset = 0;
-  ThisThread::sleep_for(1s);
-  reset = 1;
+    // // Done manually by Jack
+    // const uint8_t at_command_SL[] = {
+    //     0x7E, // Start delimeter
+    //     0x00, // LEN - MSB
+    //     0x04, // LEN - LSB
+    //
+    //     // ---- BEGIN DATA ----
+    //     0x08, // frametype
+    //     0x77, // frameid
+    //     // 0x53, 0x48, // AT command - "SH"
+    //     0x53, 0x4C, // AT command - "SL"
+    //     // (no parameter in this example)
+    //     // ---- END DATA ----
+    //
+    //     (0xFF - ((0x08 + 0x77 + 0x53 + 0x4C) & 0xFF)), // Checksum
+    //
+    // };
+    // uint8_t resp_buf[13] = {0}; // SH/SL returns an 11-byte frame
 
-  // while (1) {
-  //   // uint8_t resp_buf[10] = {0};
-  //   // char str[15] = {0};
-  //   // cs.write(0);
-  //   // spi.write(NULL, 0, (char *)resp_buf, sizeof(resp_buf));
-  //   // cs.write(1);
-  //   // to_hex(str, 15, resp_buf, 10);
-  //   // printf("%d, %s", spi_attn.read(), str);
-  //
-  //   // HAL_GPIO_WritePin(GPIOC, 8, 1);
-  //   const uint8_t local_at_command[] = {
-  //       // 0x7E, // Start delimeter
-  //       // 0x00, // LEN - MSB
-  //       // 0x04, // LEN - LSB
-  //       //
-  //       // // ---- BEGIN DATA ----
-  //       // 0x08,       // frametype
-  //       // 0x77,       // frameid
-  //       // 0x53, 0x48, // AT command - "SH"
-  //       // // (no parameter in this example)
-  //       // // ---- END DATA ----
-  //       //
-  //       // (0xFF - ((0x08 + 0x77 + 0x53 + 0x48) & 0xFF)), // Checksum
-  //       0x7E, 0x00, 0x04, 0x08, 0x17, 0x54, 0x50, 0x3C,
-  //   };
-  //
-  //   uint8_t resp_buf[10] = {0};
-  //
-  //   // cs.write(0);
-  //
-  //   // spi.write((char *)local_at_command, sizeof(local_at_command),
-  //   // printf("%d, %s", spi_attn.read(), str);
-  //
-  //   //           (char *)resp_buf, sizeof(resp_buf));
-  //
-  //   // spi.transfer(local_at_command, sizeof(local_at_command), resp_buf,
-  //   // sizeof(resp_buf), [&](int _) {
-  //   //   to_hex(str, sizeof(str), resp_buf, sizeof(resp_buf));
-  //   // });
-  //   //
-  //   // while (spi_attn.read()==1) {}
-  //   // cs.write(1);
-  //   //
-  //
-  //   if (!spi_attn) {
-  //     cs.write(0);
-  //     spi.write(NULL, 0, (char *)resp_buf, sizeof(resp_buf));
-  //     cs.write(1);
-  //   } else {
-  //     cs.write(0);
-  //     spi.write((char *)local_at_command, sizeof(local_at_command),
-  //               (char *)resp_buf, sizeof(resp_buf));
-  //     cs.write(1);
-  //   }
-  //
-  //   HAL_Delay(10);
-  //   char str[1024] = {0};
-  //   to_hex(str, sizeof(str), resp_buf, sizeof(resp_buf));
-  //
-  //   printf("0x%s, %d", str, spi_attn.read());
-  //   printf("\n");
-  // }
+    // while (!spi_attn.read()) {
+    //   printf("Waiting on spi_attn to assert...\n");
+    // }
+
+    // Write message and read response
+    cs.write(0);
+    spi.write(at_command_TP, sizeof(at_command_TP), resp_buf, sizeof(resp_buf));
+    cs.write(1);
+
+    // cs.write(0);
+    // spi.write(nullptr, 0, resp_buf, sizeof(resp_buf));
+    // cs.write(1);
+
+    // spi.write(nullptr, 0, resp_buf, sizeof(resp_buf));
+    printf("Buffer:");
+    for (uint i = 0; i < sizeof(resp_buf); i++) {
+      printf("%x ", resp_buf[i]);
+    }
+    printf("\nAttention:%d\n", spi_attn.read());
+    printf("\n");
+  }
 
   return 0;
 }
+
--- LiveTelemetryReceiver/main.cpp
+++ LiveTelemetryReceiver/main.cpp
@@ -7,71 +7,68 @@
 DigitalIn spi_attn(PB_1);
 // PB_6 is shorted to PA_6 in the default SB configuration of L432KC
 // PB 7 is shorted to PA_5 in the default SB configuration of L432KC
+SPI spi(PA_7, PA_6, PA_5); // mosi, miso, sclk[, ss]
 DigitalOut cs(PB_0);
-SPI spi(PB_5, PB_4, PB_3); // mosi, miso, sclk[, ss]
+// SPI spi(PB_5, PB_4, PB_3); // mosi, miso, sclk[, ss]
 
 int main() {
-  printf("Main()");
+  printf("Main() - LiveTelemetryReceiver");
   cs.write(1);
+
+  
 
   spi.format(8, 0);
   spi.frequency(1000000);
   spi.set_default_write_value(0x00);
 
   ThisThread::sleep_for(200ms);
-  // while ()
   while (true) {
-    // ThisThread::sleep_for(10ms);
-    const uint8_t local_at_command[] = {
-      // Done manually by Jack
-        // 0x7E, // Start delimeter
-        // 0x00, // LEN - MSB
-        // 0x04, // LEN - LSB
-        //
-        // // ---- BEGIN DATA ----
-        // 0x08,       // frametype
-        // 0x77,       // frameid
-        // 0x53, 0x48, // AT command - "SH"
-        // // (no parameter in this example)
-        // // ---- END DATA ----
-        //
-        // (0xFF - ((0x08 + 0x77 + 0x53 + 0x48) & 0xFF)), // Checksum
-      // Out of datasheet directly
-        0x7E, 0x00, 0x04, 0x08, 0x17, 0x54, 0x50, 0x3C,
+    // Example directly out of datasheet
+    const uint8_t at_command_TP[] = {
+        0x7E, 0x00, 0x04, 0x08, 0x17, 0x54, 0x50, 0x3C, // AT command - "TP"
     };
+    uint8_t resp_buf[11] = {0}; // TP returns an 11-byte frame
 
-    uint8_t resp_buf[100] = {0};
+    // // Done manually by Jack
+    // const uint8_t at_command_SL[] = {
+    //     0x7E, // Start delimeter
+    //     0x00, // LEN - MSB
+    //     0x04, // LEN - LSB
+    //
+    //     // ---- BEGIN DATA ----
+    //     0x08, // frametype
+    //     0x77, // frameid
+    //     // 0x53, 0x48, // AT command - "SH"
+    //     0x53, 0x4C, // AT command - "SL"
+    //     // (no parameter in this example)
+    //     // ---- END DATA ----
+    //
+    //     (0xFF - ((0x08 + 0x77 + 0x53 + 0x4C) & 0xFF)), // Checksum
+    //
+    // };
+    // uint8_t resp_buf[13] = {0}; // SH/SL returns an 11-byte frame
 
+    // while (!spi_attn.read()) {
+    //   printf("Waiting on spi_attn to assert...\n");
+    // }
 
     // Write message and read response
     cs.write(0);
-    spi.write(local_at_command, sizeof(local_at_command), resp_buf, sizeof(resp_buf));
+    spi.write(at_command_TP, sizeof(at_command_TP), resp_buf, sizeof(resp_buf));
     cs.write(1);
 
-    // printf("%d, %s", spi_attn.read(), str);
-    // Idea by wesley that didn't work
-    // if (!spi_attn) {
-    //   cs.write(0);
-    //   spi.write(NULL, 0, (char *)resp_buf, sizeof(resp_buf));
-    //   cs.write(1);
-    // } else {
-    //   cs.write(0);
-    //   spi.write((char *)local_at_command, sizeof(local_at_command),
-    //             (char *)resp_buf, sizeof(resp_buf));
-    //   cs.write(1);
-    // }
+    // cs.write(0);
+    // spi.write(nullptr, 0, resp_buf, sizeof(resp_buf));
+    // cs.write(1);
 
     // spi.write(nullptr, 0, resp_buf, sizeof(resp_buf));
     printf("Buffer:");
-    for(int i = 0; i < 100; i++) {
-      printf("%x ",resp_buf[i]);
+    for (uint i = 0; i < sizeof(resp_buf); i++) {
+      printf("%x ", resp_buf[i]);
     }
     printf("\nAttention:%d\n", spi_attn.read());
     printf("\n");
-
-
   }
 
   return 0;
 }
-
