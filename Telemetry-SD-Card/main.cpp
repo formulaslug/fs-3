39a1892492a370a6aea07b9abb73191b21a2e5df
Nova Mondal <rialmondal@gmail.com>
2025-05-21 20:01:06 -0700

    trying different way to write

--- Telemetry-SD-Card/main.cpp
+++ Telemetry-SD-Card/main.cpp
@@ -6,21 +6,47 @@
 #include <wchar.h>
 
 
-namespace fs = std::filesystem;
+// namespace fs = std::filesystem;
 
+// int main() {
+//   SDFileSystem sd{D2, A5, A4, D3, "sd"}; // Mosi, miso, sclk, cs
+//
+//   printf("Hello World!\n");
+//
+//   //fs::create_directory("/sd/dir2");
+//   //const fs::path file("/sd/dir2/test.txt");
+//
+//   // std::ofstream out_stream(file);
+//   // out_stream << "asjdlkajsdklajs" << std::endl;
+//
+//   printf("Goodbye World!\n");
+//
+//
+//   return 0;
+// }
+
+
+// Timer t;
 int main() {
-  SDFileSystem sd{D2, A5, A4, D3, "sd"}; // Mosi, miso, sclk, cs
+    printf("Hellow world!\n");
 
-  printf("Hello World!\n");
+    mkdir("/sd/tstdir", 0777);
 
-  fs::create_directory("/sd/dir2");
-  const fs::path file("/sd/dir2/test.txt");
+    // const uint8_t BUFF[256] = {0};
 
-  std::ofstream out_stream(file);
-  // out_stream << "asjdlkajsdklajs" << std::endl;
+    FILE *fp = fopen("/sd/tstdir/sdtest.txt", "w");
 
-  printf("Goodbye World!\n");
+    if (fp== NULL) {
+        error("Could not open file for write\n");
+    }
+    // while (1) {
+    // t.start();
+        // fwrite(BUFF, 8, 256, fp);
+    // t.stop();
+    // int64_t time = t.elapsed_time().count() / 1000;
+    // printf("Elapsed time was %lld\n", time);
+    // }
+    fclose(fp);
 
-
-  return 0;
+    printf("Goodbye world!\n");
 }
