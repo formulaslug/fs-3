6c0007a784d8578638b868b3e008262a0c2f5e59
goob10000 <65315624+goob10000@users.noreply.github.com>
2025-06-09 16:28:59 -0700

    Decoder more resiliently accounts for stray bytes not in schema

--- Telemetry-SD-Card/decoder.py
+++ Telemetry-SD-Card/decoder.py
@@ -5,7 +5,10 @@ import polars as pl
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_file','-i',type=str, action='store', help='input file name', required=True)
 parser.add_argument('--output_file','-o',type=str, action='store', help='output file name', default='out.arrow')
+parser.add_argument('--verbose','-v', action='store_true', help='verbose output')
 args = parser.parse_args()
+
+v = args.verbose
 
 types_dict = {
     "i3": np.int8,
@@ -21,7 +24,6 @@ types_dict = {
     "f6": np.float64,
     "b0": bool
     }
-
 
 with open (args.input_file, "rb" ) as f:
     # f = open("test.fsdaq", "rb")
@@ -47,8 +49,13 @@ with open (args.input_file, "rb" ) as f:
         colTypes.append((types_dict[colType], int((2**int(colType[1]))/8*n)))
         pos += 2
     # print(colTypes)
-    # print(len(data))
-    while(ascii(data[pos:pos+8]) != header):
+    data_left = len(data[pos:])
+    len_of_frame = sum([x[1] for x in colTypes])
+    chunks = np.floor(data_left / len_of_frame)
+    misc_bytes = (data_left % len_of_frame) - 8
+    print(f"chunks left: {chunks}")
+    print(f"misc bytes left: {misc_bytes}")
+    for i in range(int(chunks)):
         frame_pieces = []
         for i in range(m):
             col_byte_len = colTypes[i][1]
@@ -57,17 +64,27 @@ with open (args.input_file, "rb" ) as f:
             #     df.write_csv(args.output_file)
             #     exit(0)
             if col_type != bool:
+                if(v):
+                    print(f"n = {n}; col_byte_len = {col_byte_len}; col_type = {col_type}")
                 col_bit = np.frombuffer(data[pos:pos+col_byte_len], dtype=col_type, count=n)
             else:
-                col_bit = np.frombuffer(np.unpackbits(data[pos:pos+col_byte_len]), dtype=bool, count=n)
+                # print(f"n = {n}; int(n/8) = {int(n/8)}")
+                if(v):
+                    print(f"type = {col_type}")
+                col_bit = np.frombuffer(data[pos:pos+col_byte_len], dtype=np.uint8, count=int(n/8))
+                col_bit1 = np.unpackbits(col_bit)
+                col_bit = [True if x == 1 else False for x in col_bit1]
+            if(v):
+                print(f"col_bit: {col_bit}")
             frame_piece_series = pl.Series(colTitles[i], col_bit)
             frame_pieces.append(frame_piece_series)
             pos += col_byte_len
         frame_piece = pl.DataFrame(frame_pieces)
-        # print(frame_pieces)
-        if df.is_empty():
-            df = frame_piece
-        else:
-            df = df.vstack(frame_piece)
+    # print(frame_pieces)
+    if df.is_empty():
+        df = frame_piece
+    else:
+        df = df.vstack(frame_piece)
         # print(df)
+    print(f"Stuff Left: {ascii(data[pos:])}")
     df.write_csv(args.output_file)
---
+++ Telemetry-SD-Card/temp.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+np.unpackbits(np.array([0,1,2], dtype=np.uint8))
