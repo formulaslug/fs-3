b99ddf01a77fd5bc807300e1d0208d1849e98652
Jack N <nystromjp@gmail.com>
2025-06-09 18:57:06 -0700

    Save

--- Telemetry-SD-Card/main.cpp
+++ Telemetry-SD-Card/main.cpp
@@ -11,24 +11,25 @@ void error_quit(std::string msg) {
 int main(int argc, char *argv[]) {
     printf("Hello World!\n");
 
-    SDFileSystem sd{D2, A5, A4, D3, "sd"}; // Mosi, miso, sclk, cs // L423KC
-    // SDFileSystem sd{D11, D12, D13, D10, "sd"}; // Mosi, miso, sclk, cs // F446RE
-    if (sd.disk_initialize() != 0)
+    // SDFileSystem sd{D2, A5, A4, D3, "sd"}; // Mosi, miso, sclk, cs // L423KC
+    SDFileSystem sd{D11, D12, D13, D10, "sd"}; // Mosi, miso, sclk, cs // F446RE
+    if (sd.disk_initialize() != 0) {
         error_quit("Failed to initialize SD card!");
+    }
 
     mkdir("/sd/fsdaq", 0777);
 
-    FILE *file = fopen("/sd/arrow/test.fsdaq", "w");
-    if (file == NULL)
+    FILE *file = fopen("/sd/fsdaq/test.fsdaq", "w");
+    if (file == NULL) {
         error_quit("Error opening file!");
-
+    }
 
     fwrite("FSDAQ001", 8, 1, file);
 
     write_fsdaq_schema(file);
 
     Values vals{};
-    memset(&vals, 1, sizeof(vals));
+    // memset(&vals, INT_MAX, sizeof(vals));
 
     write_fsdaq_batch(&vals, file);
 
@@ -37,5 +38,6 @@ int main(int argc, char *argv[]) {
     fclose(file);
 
     printf("Goodbye World!\n");
-    while (1) {};
+    while (1) {
+    };
 }
