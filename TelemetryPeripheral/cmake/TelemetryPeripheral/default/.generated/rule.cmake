0285200d0cb838d6638889582f5c3220525b85a5
Jack N <nystromjp@gmail.com>
2025-07-27 12:42:48 -0700

    remove old TelemetryPeripheral and Telemetry-MainCANTest folders

--- TelemetryPeripheral/cmake/TelemetryPeripheral/default/.generated/rule.cmake
+++
@@ -1,4 +0,0 @@
-# The following functions contains all the flags passed to the different build stages.
-
-set(PACK_REPO_PATH "/Users/novamondal/.mchp_packs" CACHE PATH "Path to the root of a pack repository.")
-
