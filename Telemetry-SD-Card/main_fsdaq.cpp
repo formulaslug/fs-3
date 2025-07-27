544896c932f2be28e14fd7eec36daff5b64211b9
Jack N <nystromjp@gmail.com>
2025-07-27 12:31:51 -0700

    get SD card to work on the car lol

--- Telemetry-SD-Card/main_fsdaq.cpp
+++ Telemetry-SD-Card/main_fsdaq.cpp
@@ -1,3 +1,4 @@
+#include "./fsdaq_encoder_generated_from_dbc.hpp"
 #include "FATFileSystem.h"
 #include "SDBlockDevice.h"
 #include "mbed.h"
@@ -15,6 +16,24 @@ SDBlockDevice sd{
 // "sd" is the name of the filesystem; i.e. filepaths are /sd/...
 FATFileSystem fs{"sd"};
 
+// Our data
+Values vals{};
+ValuesRow current_row{};
+
+// Debug
+mbed_stats_heap_t heap;
+mbed_stats_stack_t stack;
+
+void print_mem_usage() {
+    mbed_stats_heap_get(&heap);
+    printf("\nHeap: %u/%u (used/reserved), max usage: %u, allocs: %u\n",
+           heap.current_size, heap.reserved_size, heap.max_size,
+           heap.alloc_cnt);
+    mbed_stats_stack_get(&stack);
+    printf("Stack: max usage: %u, bytes reserved: %u\n\n", stack.max_size,
+           stack.reserved_size);
+}
+
 void error_quit(std::string msg) {
     printf("%s\n", msg.c_str());
     while (1) {
@@ -30,6 +49,8 @@ int main(int argc, char *argv[]) {
     // TODO: experiment with async SPI and/or DMA
     // sd.set_async_spi_mode(true, DMAUsage::DMA_USAGE_ALWAYS);
 
+    print_mem_usage();
+
     // Mount the FATFileSystem (so we can use regular C file IO like fopen/read)
     int error = fs.mount(&sd);
     if (error) {
@@ -40,14 +61,35 @@ int main(int argc, char *argv[]) {
         if (error) error_quit("Error: could not reformat SD card! Is the SD card plugged in?\n");
     }
 
-    mkdir("/sd/testing", 0777);
+    mkdir("/sd/fsdaqqqq", 0777);
 
-    FILE *file = fopen("/sd/testing/test.txt", "w+");
+    print_mem_usage();
+
+    FILE *file = fopen("/sd/fsdaqqqq/test.fsdaq", "w+");
     if (file == NULL) {
         error_quit("Error opening file!");
     }
 
-    fwrite("alksjdlkasjdklasjflka Hellow  World!!!", 100, 1, file);
+    fwrite("FSDAQ001", 8, 1, file);
+
+    write_fsdaq_schema(file);
+
+    print_mem_usage();
+
+
+    for (int i=0; i<5; i++) {
+        memset(&current_row, INT_MAX, sizeof(current_row));
+        vals.setRow(current_row, i);
+    }
+    write_fsdaq_batch(&vals, file);
+    write_fsdaq_batch(&vals, file);
+    write_fsdaq_batch(&vals, file);
+    write_fsdaq_batch(&vals, file);
+    write_fsdaq_batch(&vals, file);
+
+    print_mem_usage();
+
+    fwrite("FSDAQ001", 8, 1, file);
 
     fclose(file);
 
