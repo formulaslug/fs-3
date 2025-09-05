38efad80c8355016d0937fe686df5661c8fd0483
Jack N <nystromjp@gmail.com>
2025-09-04 19:47:03 -0700

    extremely controversion change

--- Telemetry-Main/fsdaq/data_logger.hpp
+++ Telemetry-Main/fsdaq/data_logger.hpp
@@ -20,8 +20,9 @@ public:
      * batch.
      */
     void append_row(fsdaq::DataRow& next_row);
+
 private:
-    /* 
+    /*
      * Initializes the SD Card block device, mounts the FATFileSystem, and opens
      * a new fsdaq file.
      */
@@ -33,16 +34,16 @@ private:
 
     // SDBlockDevice - lowest-level interfaces with the SD card.
     // Pin configurations and transfer speeds are set in mbed_app.json.
-    SDBlockDevice sd;
+    SDBlockDevice& sd;
     // FATFileSystem - Creates a FAT filesystem on the SDBlockDevice.
     // "sd" is the name of the filesystem; i.e. filepaths are /sd/...
     FATFileSystem fatfs;
     // File pointer to the file opened on the SD card
-    FILE *sd_fp;
+    FILE* sd_fp;
 
     // The current batch of data that we're appending rows to.
     fsdaq::DataBatch current_batch{};
     int row_idx;
 };
 
-}
+} // namespace fsdaq
