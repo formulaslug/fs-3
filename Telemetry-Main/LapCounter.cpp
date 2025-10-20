d89c8e2634ee302da2eee7b7709a7937d94abc54
Nicholas Tang <ntang11@ucsc.edu>
2025-10-20 15:43:52 -0700

    add minimum time check for a lap

--- Telemetry-Main/LapCounter.cpp
+++ Telemetry-Main/LapCounter.cpp
@@ -6,6 +6,7 @@
 #define X_TOLERANCE 8.0f        // check lap completion if x is within +- X_TOLERANCE meters of home x coord
 #define Y_TOLERANCE 8.0f        // check lap completion if y is within +- Y_TOLERANCE meters of home y coord
 #define HEADING_TOLERANCE 30.0f // check lap completion if heading is within +- HEADING_TOLERANCE degrees of home
+#define MIN_LAP_TIME 10         // minimum lap time in seconds. used to ensure same lap isn't counted twice
 
 LapCounter::LapCounter(VehicleState state) {
     resetLapCounter(state);
@@ -30,40 +31,54 @@ void LapCounter::resetLapCounter(VehicleState state) {
     data.lap_curr_y_f = data.lap_home_y_f;
     data.lap_curr_heading_f = data.lap_home_heading_f;
 
+    data.lap_counter = 0;
+
     timer.reset();
     timer.start();
 }
 
 void LapCounter::updateLapCounter(VehicleState state) {
     //  Weighted avg for prev to average out noise 
-    data.lap_prev_latitude_f = data.lap_prev_latitude_f / 4.0f + 0.75f * data.lap_curr_latitude_f;
-    data.lap_prev_longitude_f = data.lap_prev_longitude_f / 4.0f + 0.75f * data.lap_curr_longitude_f;
-    data.lap_prev_x_f = data.lap_prev_x_f / 4.0f + 0.75f * data.lap_curr_x_f;
-    data.lap_prev_y_f = data.lap_prev_y_f / 4.0f + 0.75f * data.lap_curr_y_f;
-    data.lap_prev_heading_f = data.lap_prev_heading_f / 4.0f + 0.75f * data.lap_curr_heading_f;
+    data.lap_prev_latitude_f = 0.25f * data.lap_prev_latitude_f + 0.75f * data.lap_curr_latitude_f;
+    data.lap_prev_longitude_f = 0.25f * data.lap_prev_longitude_f + 0.75f * data.lap_curr_longitude_f;
+    data.lap_prev_x_f = 0.25f * data.lap_prev_x_f + 0.75f * data.lap_curr_x_f;
+    data.lap_prev_y_f = 0.25f * data.lap_prev_y_f + 0.75f * data.lap_curr_y_f;
+    data.lap_prev_heading_f = 0.25f * data.lap_prev_heading_f + 0.75f * data.lap_curr_heading_f;
 
     //  Get curr data, transform to local coordinate plane
     data.lap_curr_latitude_f = state.vdmGpsLatLong.LATITUDE;
     data.lap_curr_longitude_f = state.vdmGpsLatLong.LONGITUDE;
     data.lap_curr_x_f = EARTH_RADIUS * (data.lap_curr_longitude_f -
-            data.lap_home_longitude_f) * cos(data.lap_home_longitude_f);
+            data.lap_home_longitude_f) * cos(M_PI * data.lap_home_latitude_f /
+                180);
     data.lap_curr_y_f = EARTH_RADIUS * (data.lap_curr_latitude_f -
             data.lap_home_latitude_f);
     //  TRANSFORM: Apply rotation matrix, home heading is x axis.
-    data.lap_curr_x_f = data.lap_curr_x_f * cos(data.lap_home_heading_f) +
-        data.lap_curr_y_f * sin(data.lap_home_heading_f);
-    data.lap_curr_y_f = data.lap_curr_x_f * -sin(data.lap_home_heading_f) +
-        data.lap_curr_y_f * cos(data.lap_home_heading_f);
+    double home_heading_rad_f = M_PI * data.lap_home_heading_f / 180;           // convert to rad for trig funcs
+    double temp_x_f = data.lap_curr_x_f * cos(home_heading_rad_f) + data.lap_curr_y_f * sin(home_heading_rad_f);
+    double temp_y_f = data.lap_curr_x_f * -sin(home_heading_rad_f) + data.lap_curr_y_f * cos(home_heading_rad_f);
+    data.lap_curr_x_f = temp_x_f;
+    data.lap_curr_y_f = temp_y_f;
+
     data.lap_curr_heading_f = (float) (state.vdmGpsData.TRUE_COURSE % 36000) * 0.01f; //scale to 0-359.99
 
     //  If nowhere near home coords/heading, return immediately
     if (abs(data.lap_curr_x_f - data.lap_home_x_f) > X_TOLERANCE) return;
     if (abs(data.lap_curr_y_f - data.lap_home_y_f) > Y_TOLERANCE) return;
     if (abs(data.lap_curr_heading_f - data.lap_home_heading_f) > HEADING_TOLERANCE) return;
+    if (timer.elapsed_time().count() < MIN_LAP_TIME) return;
 
     //  If close enough and crossed home line, increment counter
     if (data.lap_curr_x_f > data.lap_home_x_f && data.lap_prev_x_f < data.lap_home_x_f) {
         data.lap_counter++;
+        data.lap_time = timer.elapsed_time();
+
+        timer.reset();
+        timer.start();
         return;
     }
+}
+
+std::chrono::microseconds LapCounter::getTime() {
+    return timer.elapsed_time();
 }
--- Telemetry-Main/LapCounter.hpp
+++ Telemetry-Main/LapCounter.hpp
@@ -1,3 +1,13 @@
+/**
+ * Lap Counter
+ * Author: Nicholas Tang
+ *
+ * Made to separate lap counting from VSM. Uses VSM GPS and HEADING/TRUE_COURSE
+ * data. Make sure to call updateLapCounter a lot.
+ *
+ * Note: "local coordinate plane" is defined as: nose towards positive x
+ */
+
 #ifndef LAP_COUNTER_HPP
 #define LAP_COUNTER_HPP
 
@@ -8,29 +18,34 @@
 struct LapCounterData {
     float lap_home_latitude_f;
     float lap_home_longitude_f;
-    float lap_home_x_f;
-    float lap_home_y_f;
+    float lap_home_x_f;                 // relative to local coordinate plane
+    float lap_home_y_f;                 // relative to local coordinate plane
     float lap_home_heading_f;
 
     float lap_prev_latitude_f;
     float lap_prev_longitude_f;
-    float lap_prev_x_f;
-    float lap_prev_y_f;
+    float lap_prev_x_f;                 // relative to local coordinate plane
+    float lap_prev_y_f;                 // relative to local coordinate plane
     float lap_prev_heading_f;
 
     float lap_curr_latitude_f;
     float lap_curr_longitude_f;
-    float lap_curr_x_f;
-    float lap_curr_y_f;
+    float lap_curr_x_f;                 // relative to local coordinate plane
+    float lap_curr_y_f;                 // relative to local coordinate plane
     float lap_curr_heading_f;
 
     uint8_t lap_counter;                // MARK: surely we don't do more than 255 laps(?)
+    std::chrono::microseconds lap_time;
 };
 
 class LapCounter {
     public:
         LapCounterData data;
         Timer timer;
+
+        /**
+         * Constructor, initializes all fields to 0 but lat/long
+         */
         LapCounter(VehicleState state);
         /**
          * Resets object field. GPS data is set to wherever is in vehicle state
@@ -40,6 +55,10 @@ class LapCounter {
          * Checks if a lap has been completed. If so, increments lap counter
          */
         void updateLapCounter(VehicleState state);
+        /**
+         * Gets the lap time in microseconds. 
+         */
+        std::chrono::microseconds getTime();
 };
 
 #endif
